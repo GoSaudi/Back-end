8f8df0d7bc7179ded463724bb245760bf06193d9
mako-admin <devops@makoitlab.com>
2024-09-26 19:32:59 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-cd /home/ubuntu/go-saudi-be
+git pull 
+# export IMAGE_TAG=$(git rev-parse --short HEAD)
+# echo $IMAGE_TAG
+docker ps
 docker-compose down
-git pull origin develop --no-commit
-docker rmi 639040537604.dkr.ecr.ap-south-1.amazonaws.com/go-saudi-be:latest
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 639040537604.dkr.ecr.ap-south-1.amazonaws.com
-docker login -u AWS -p $(aws ecr get-login-password --region ap-south-1) 639040537604.dkr.ecr.ap-south-1.amazonaws.com
-docker pull 639040537604.dkr.ecr.ap-south-1.amazonaws.com/go-saudi-be:latest
-docker-compose up -d --build
-sudo systemctl restart nginx
 docker system prune -a -f
-
-
+docker-compose up -d --build
+clear
+docker ps
